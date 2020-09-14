18f26ad4512234f4ae886dedcadd0858fa509dea
Matthew Apuya <m.apuya19@gmail.com>
2020-09-14 06:11:39 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7abd9716f96f5c9c8748.js"
+    "url": "webpack-runtime-1affc1074f71bfc76f7b.js"
   },
   {
     "url": "framework-b5b229d58c3c710d5429.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7ce94cb3a58771dea633856174ac02ef"
+    "revision": "f1e4a4223d022055cd088f01ab8643f2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "fbf4f07f5b161df5ebd5f849a34ed0de"
+    "revision": "1cf0e71f40baf4f4a898ed96fddb07d2"
   },
   {
     "url": "polyfill-3370241ddb89d1467de6.js"
