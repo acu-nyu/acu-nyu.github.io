ac342142b3bd09b8016c3fbbf7b4dab2635c763e
Matthew Apuya <m.apuya19@gmail.com>
2022-10-11 19:14:27 -0400

    update newbies / rip harry

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -23,6 +23,14 @@ import grace from '../assets/images/e-board/grace.jpg';
 import yujin from '../assets/images/e-board/yujin.jpg';
 import jocelyn from '../assets/images/e-board/jocelyn.jpg';
 import doyeon from '../assets/images/e-board/doyeon.jpg';
+import kyle from '../assets/images/e-board/kyle.jpg';
+import jake from '../assets/images/e-board/jake.jpg';
+import avery from '../assets/images/e-board/avery.jpg';
+import phoebe from '../assets/images/e-board/phoebe.jpg';
+import andy from '../assets/images/e-board/andy.jpg';
+// import tiffany from '../assets/images/e-board/tiffany.jpg';
+import lina from '../assets/images/e-board/lina.jpg';
+// import hailey from '../assets/images/e-board/hailey.jpg';
 
 const IndexPage = () => (
   <Layout fullMenu>
@@ -156,12 +164,12 @@ const IndexPage = () => (
               <h3 className="major">Elaina Feng</h3>
             </article>
 
-            <article>
+            {/* <article>
               <div className="image">
                 <img src={harry} alt="" />
               </div>
               <h3 className="major">Harry Li</h3>
-            </article>
+            </article> */}
 
             <article>
               <div className="image">
@@ -183,6 +191,62 @@ const IndexPage = () => (
               </div>
               <h3 className="major">Doyeon Kim</h3>
             </article>
+
+            <article>
+              <div className="image">
+                <img src={kyle} alt="" />
+              </div>
+              <h3 className="major">Kyle Park</h3>
+            </article>
+
+            <article>
+              <div className="image">
+                <img src={jake} alt="" />
+              </div>
+              <h3 className="major">Jake Tran</h3>
+            </article>
+
+            <article>
+              <div className="image">
+                <img src={andy} alt="" />
+              </div>
+              <h3 className="major">Andy Cabindol</h3>
+            </article>
+
+            <article>
+              <div className="image">
+                <img src={avery} alt="" />
+              </div>
+              <h3 className="major">Avery Teano</h3>
+            </article>
+
+            <article>
+              <div className="image">
+                <img src={phoebe} alt="" />
+              </div>
+              <h3 className="major">Phoebe Huang</h3>
+            </article>
+
+            {/* <article>
+              <div className="image">
+                <img src={tiffany} alt="" />
+              </div>
+              <h3 className="major">Tiffany Lee</h3>
+            </article> */}
+
+            <article>
+              <div className="image">
+                <img src={lina} alt="" />
+              </div>
+              <h3 className="major">Lina Gallo</h3>
+            </article>
+
+            {/* <article>
+              <div className="image">
+                <img src={hailey} alt="" />
+              </div>
+              <h3 className="major">Hailey Kim</h3>
+            </article> */}
           </section>
         </div>
       </div>
