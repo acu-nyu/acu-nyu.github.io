20a313f8a68a295b2ebd2eb36a3a817dde48224a
axie22 <alexxie9667@gmail.com>
2023-09-07 00:23:16 -0400

    updated team photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import logo from '../assets/images/logo.png';
 import icon1 from '../assets/images/icon1.jpg';
 import icon2 from '../assets/images/icon2.jpg';
 import icon3 from '../assets/images/icon3.jpg';
-import f22team from '../assets/images/f22-team.jpg';
+import s23team from '../assets/images/s23-team.jpg';
 
 import event_smash from '../assets/images/events/event_smash.jpg';
 import event_palentines from '../assets/images/events/event_palentines.jpg';
@@ -98,7 +98,7 @@ const IndexPage = () => (
       <section id="four" className="wrapper alt spotlight style5">
         <div className="inner">
           <div className="image">
-            <img src={f22team} alt="" />
+            <img src={s23team} alt="" />
           </div>
           <div className="content">
             <h2 className="major">Who can join?</h2>
