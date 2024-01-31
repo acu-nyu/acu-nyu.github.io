eba6651441d7638d39dd52e5eadef6d001c67ec2
axie22 <alexxie9667@gmail.com>
2024-01-30 22:50:58 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-12c0625785fb6757ca03.js"
+    "url": "webpack-runtime-97f396d8e2447079b65f.js"
   },
   {
     "url": "framework-7eaec00a8cd82505bc79.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "744f6a6e44d53041be92db93989d8a13"
+    "revision": "fdf34715557a4ea6705d4ad457ef2d19"
   },
   {
     "url": "manifest.webmanifest",
